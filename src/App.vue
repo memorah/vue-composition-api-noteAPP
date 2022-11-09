b106f1c064dd3bc5733be7f3239dfa3db6f6ffd2
memorah <filip.mackov@gmail.com>
2022-11-09 22:04:07 +0100

    edit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <router-link to="/">Notes</router-link>
-  <router-link to="/stats">Statssss</router-link>
+  <router-link to="/stats">Statsssss</router-link>
   <RouterView />
 </template>
 

747780e1804065276d781892495ebd14af6115d2
memorah <filip.mackov@gmail.com>
2023-01-05 16:31:58 +0100

    edit test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <router-link to="/">Notes</router-link>
   <router-link to="/stats">Statsaaaaasss</router-link>
+  <router-link to="/test">Statsaaaaasss</router-link>
   <RouterView />
 </template>
 

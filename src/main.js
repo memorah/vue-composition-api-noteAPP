ebe0d9ee5264283a44bbf8183d9025c9543b61eb
memorah <filip.mackov@gmail.com>
2023-01-05 16:39:54 +0100

    edit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import { createRouter, createWebHashHistory } from "vue-router";
 import ViewNotes from "@/views/ViewNotes.vue";
 import ViewStats from "@/views/ViewStats.vue";
+import VueTesting from "@/views/VueTesting.vue";
 import App from "./App.vue";
 
 import "./assets/main.css";
@@ -18,6 +19,11 @@ const router = createRouter({
       path: "/stats",
       name: "stats",
       component: ViewStats,
+    },
+    {
+      path: "/test",
+      name: "test",
+      component: VueTesting,
     },
   ],
 });

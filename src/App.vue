24eb117178de4c3153a1e784ead39b8bf7de818f
memorah <filip.mackov@gmail.com>
2023-01-09 15:45:23 +0100

    edit

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
   <router-link to="/stats">Statsaaaaasss</router-link>
   <router-link to="/test">Button 1</router-link>
   <router-link to="/button2">Button 2</router-link>
+  <router-link to="/button3">Button 3</router-link>
   <RouterView />
 </template>
 

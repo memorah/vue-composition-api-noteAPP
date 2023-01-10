645100db08f4335263399ac970a5b52e35e9119a
memorah <filip.mackov@gmail.com>
2023-01-10 14:25:26 +0100

    btn5

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
   <router-link to="/button2">Button 2</router-link>
   <router-link to="/button3">Button 3</router-link>
   <router-link to="/button4">Button 4</router-link>
+  <router-link to="/button4">Button 5</router-link>
   <RouterView />
 </template>
 

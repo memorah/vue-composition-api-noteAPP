7ce39d23c0647bc7effdf9828b30f3fdeea6e230
memorah <filip.mackov@gmail.com>
2023-01-10 16:35:13 +0100

    edit nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,5 @@
 <template>
   <NavBar />
-  <router-link to="/" class="button">Notes</router-link>
-  <router-link to="/stats" class="button">Statsaaaaasss</router-link>
-  <router-link to="/test">Button 1</router-link>
-  <router-link to="/button2">Button 2</router-link>
-  <router-link to="/button3">Button 3</router-link>
-  <router-link to="/button4">Button 4</router-link>
-  <router-link to="/button5">Button 5</router-link>
-  <router-link to="/button6">Button 6</router-link>
   <RouterView />
 </template>
 

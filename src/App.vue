d0da6d38d3a651f905b9de988e450b3eefb78558
memorah <filip.mackov@gmail.com>
2023-01-10 14:22:18 +0100

    edit app

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
   <router-link to="/test">Button 1</router-link>
   <router-link to="/button2">Button 2</router-link>
   <router-link to="/button3">Button 3</router-link>
+  <router-link to="/button4">Button 4</router-link>
   <RouterView />
 </template>
 

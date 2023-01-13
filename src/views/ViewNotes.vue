db07c30db689ed3736e60b956f9060993dcc5fc2
memorah <filip.mackov@gmail.com>
2023-01-13 11:14:07 +0100

    disabled btn

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -3,12 +3,22 @@
     <div class="div card has-background-succes p-4 mb-4">
       <div class="field">
         <div class="control">
-          <textarea class="textarea" placeholder="Add a new note..."></textarea>
+          <textarea
+            v-model="newNote"
+            class="textarea"
+            placeholder="Add a new note..."
+          ></textarea>
         </div>
       </div>
       <div class="field is-grouped is-grouped-right">
         <div class="control">
-          <button class="button is-link has-background-success">Add New</button>
+          <button
+            @click="addNote"
+            :disabled="!newNote"
+            class="button is-link has-background-success"
+          >
+            Add New
+          </button>
         </div>
       </div>
     </div>

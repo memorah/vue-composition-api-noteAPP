6d74ecb548092e3548f7f6204883fbf587279ae2
memorah <filip.mackov@gmail.com>
2023-01-13 10:51:33 +0100

    add form

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="notes">
+    <div class="div card has-background-succes p-4 mb-4">
+      <div class="field">
+        <div class="control">
+          <textarea class="textarea" placeholder="Add a new note..."></textarea>
+        </div>
+      </div>
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button class="button is-link has-background-success">Add New</button>
+        </div>
+      </div>
+    </div>
     <div v-for="i in 3" class="card mb-4">
       <div class="card-content">
         <div class="content">

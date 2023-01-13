f8dcb1113999a4e84d2642b7aac9ae1c1c8bf2cc
memorah <filip.mackov@gmail.com>
2023-01-13 13:01:18 +0100

    child comp

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -22,23 +22,14 @@
           </button>
         </div>
       </div>
-    </div>
-    <div v-for="note in notes" :key="note.id" class="card mb-4">
-      <div class="card-content">
-        <div class="content">
-          {{ note.content }}
-        </div>
-      </div>
-      <footer class="card-footer">
-        <a href="#" class="card-footer-item">Edit</a>
-        <a href="#" class="card-footer-item">Delete</a>
-      </footer>
+      <Note v-for="note in notes" :key="note.id" />
     </div>
   </div>
 </template>
 <script setup>
 /* imports */
 import { ref } from "vue";
+import Note from "@/components/Notes/NoteComponent.vue";
 /* notes*/
 const newNote = ref("");
 const newNoteRef = ref(null);

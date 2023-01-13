a7db2be36f88db7cb5bec418022f3082ac668e3e
memorah <filip.mackov@gmail.com>
2023-01-13 10:41:45 +0100

    add card

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="notes">
-    <h1>Notes</h1>
+    <div class="card">
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+          iaculis mauris. Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit. Phasellus nec iaculis mauris.
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
   </div>
 </template>

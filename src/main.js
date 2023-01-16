a71de81979fd8a88d53f53168c0ca02701101a7c
memorah <filip.mackov@gmail.com>
2023-01-16 15:44:03 +0100

    import pinia main js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import router from "@/router/index.js";
 import App from "./App.vue";
 import "./assets/main.css";
-createApp(App).use(router).mount("#app");
+
+const pinia = createPinia();
+
+createApp(App).use(router).use(pinia).mount("#app");

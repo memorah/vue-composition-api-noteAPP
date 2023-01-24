922049209f46fa1c602581d73c24a4992ba93225
memorah <filip.mackov@gmail.com>
2023-01-24 21:31:17 +0100

    Edit nOte router

---
+++ src/views/ViewEditNote.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="edit-note">
+    <h1>Edit Note</h1>
+  </div>
+</template>
+<script setup>
+/* imports */
+import { ref } from "vue";
+import { useStoreNotes } from "@/stores/storeNotes";
+/*store*/
+const storeNotes = useStoreNotes();
+/* notes*/
+</script>

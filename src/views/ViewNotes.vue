d1b0b1addba2450521ef90ed334fa42023133b91
memorah <filip.mackov@gmail.com>
2023-02-14 15:01:24 +0100

    edit

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -7,7 +7,7 @@
           :disabled="!newNote"
           class="button is-link has-background-success"
         >
-          Add Note
+          Add Notes
         </button>
       </template>
     </AddEditNote>

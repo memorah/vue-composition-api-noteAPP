297db90df9570e3439305dcbf4f6f0d968105465
memorah <filip.mackov@gmail.com>
2023-02-17 11:05:26 +0100

    editNote page

--- src/views/ViewEditNote.vue
+++ src/views/ViewEditNote.vue
@@ -1,30 +1,17 @@
 <template>
   <div class="edit-note">
-    <h1>Edit Note with an id of: {{ $route.params.id }}</h1>
-    <div class="div card has-background-succes p-4 mb-4">
-      <div class="field">
-        <div class="control">
-          <textarea
-            v-model="newNote"
-            class="textarea"
-            placeholder="Add a new note..."
-            ref="newNoteRef"
-          ></textarea>
-        </div>
-      </div>
-      <div class="field is-grouped is-grouped-right">
-        <div class="control">
-          <button
-            @click="addNote"
-            :disabled="!newNote"
-            class="button is-link has-background-success"
-          >
-            Add Notes
-          </button>
-        </div>
-      </div>
-      <Note v-for="note in storeNotes.notes" :key="note.id" :note="note" />
-    </div>
+    <AddEditNote v-model="noteContent" ref="addEditNoteRef">
+      <template #buttons>
+        <button class="button is-link has-background-success">Save Note</button>
+      </template>
+    </AddEditNote>
   </div>
 </template>
-<script setup></script>
+<script setup>
+/* import */
+import { ref } from "vue";
+import AddEditNote from "@/components/Notes/AddEditNote.vue";
+
+/* note */
+const noteContent = ref("");
+</script>
